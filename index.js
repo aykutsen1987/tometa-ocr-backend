f106818925bd77bac5ac49b13284e45007b8d104
aykutsen1987 <aykutsen1987@gmail.com>
2026-01-15 15:01:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("✅ ToMeta OCR Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log("🚀 OCR server started on port", PORT);
+});
 
